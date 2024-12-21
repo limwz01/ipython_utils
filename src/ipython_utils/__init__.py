580b671193c20e9485eef592bb32d9fd81cdfb6c
limwz01 <117669574+limwz01@users.noreply.github.com>
2024-12-22 00:50:51 +0800

    add embed_kernel, reload_module and setup_embedded_shell to API

--- src/ipython_utils/__init__.py
+++ src/ipython_utils/__init__.py
@@ -1,1 +1,3 @@
-from .core import _ipy_magic_inner, add_except_hook, embed, embed2, try_all_statements, run_func
+from .core import (_ipy_magic_inner, add_except_hook, embed, embed2,
+                   embed_kernel, reload_module, run_func, setup_embedded_shell,
+                   try_all_statements)
